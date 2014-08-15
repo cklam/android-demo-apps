27bef2e3c588c0e2351294a7fdc6418240af4bd4
Hugo <hudomju@gmail.com>
2014-08-15 10:49:16 +0200

    Step 4: Initialize Android Relayr Sdk

---
+++ thermometer/src/main/java/io/relayr/demo/thermometer/ThermometerDemoApplication.java
@@ -0,0 +1,12 @@
+package io.relayr.demo.thermometer;
+
+import android.app.Application;
+
+public class ThermometerDemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RelayrSdkInitializer.initSdk(this);
+    }
+}
