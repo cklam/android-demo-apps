89b17e852ba7e48923ff16721b972f82dcbd65e1
Bernard Pletikosa <bernard.pletikosa@gmail.com>
2015-03-04 23:53:56 +0100

    Change way of reading web socket data

--- thermometer/src/main/java/io/relayr/demo/thermometer/ThermometerDemoActivity.java
+++ thermometer/src/main/java/io/relayr/demo/thermometer/ThermometerDemoActivity.java
@@ -8,12 +8,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import io.relayr.LoginEventListener;
 import io.relayr.RelayrSdk;
 import io.relayr.model.DeviceModel;
 import io.relayr.model.Reading;
@@ -21,6 +18,7 @@ import io.relayr.model.Transmitter;
 import io.relayr.model.TransmitterDevice;
 import io.relayr.model.User;
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -28,7 +26,7 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.Subscriptions;
 
-public class ThermometerDemoActivity extends Activity implements LoginEventListener {
+public class ThermometerDemoActivity extends Activity {
 
     private TextView mWelcomeTextView;
     private TextView mTemperatureValueTextView;
@@ -40,24 +38,30 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         View view = View.inflate(this, R.layout.activity_thermometer_demo, null);
         mWelcomeTextView = (TextView) view.findViewById(R.id.txt_welcome);
         mTemperatureValueTextView = (TextView) view.findViewById(R.id.txt_temperature_value);
         mTemperatureNameTextView = (TextView) view.findViewById(R.id.txt_temperature_name);
         setContentView(view);
-        if (!RelayrSdk.isUserLoggedIn()) {
-            RelayrSdk.logIn(this, this);
+        if (RelayrSdk.isUserLoggedIn()) {
+            updateUiForALoggedInUser();
+        } else {
+            updateUiForANonLoggedInUser();
+            logIn();
         }
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         menu.clear();
+
         if (RelayrSdk.isUserLoggedIn()) {
             getMenuInflater().inflate(R.menu.thermometer_demo_logged_in, menu);
         } else {
             getMenuInflater().inflate(R.menu.thermometer_demo_not_logged_in, menu);
         }
+
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -67,13 +71,38 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         if (item.getItemId() == R.id.action_log_in) {
-            RelayrSdk.logIn(this, this);
+            logIn();
             return true;
         } else if (item.getItemId() == R.id.action_log_out) {
             logOut();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void logIn() {
+        RelayrSdk.logIn(this)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(ThermometerDemoActivity.this,
+                                R.string.unsuccessfully_logged_in, Toast.LENGTH_SHORT).show();
+                        updateUiForANonLoggedInUser();
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Toast.makeText(ThermometerDemoActivity.this,
+                                R.string.successfully_logged_in, Toast.LENGTH_SHORT).show();
+                        invalidateOptionsMenu();
+                        updateUiForALoggedInUser();
+                    }
+                });
     }
 
     private void logOut() {
@@ -109,6 +138,7 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
                     public void onError(Throwable e) {
                         Toast.makeText(ThermometerDemoActivity.this, R.string.something_went_wrong,
                                 Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
                     }
 
                     @Override
@@ -131,7 +161,8 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
                         // kinds of transmitter.
                         if (transmitters.isEmpty())
                             return Observable.from(new ArrayList<List<TransmitterDevice>>());
-                        return RelayrSdk.getRelayrApi().getTransmitterDevices(transmitters.get(0).id);
+                        return RelayrSdk.getRelayrApi().getTransmitterDevices(transmitters.get(0)
+                                .id);
                     }
                 })
                 .subscribeOn(Schedulers.io())
@@ -145,6 +176,7 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
                     public void onError(Throwable e) {
                         Toast.makeText(ThermometerDemoActivity.this, R.string.something_went_wrong,
                                 Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
                     }
 
                     @Override
@@ -166,13 +198,13 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
     }
 
     private void unSubscribeToUpdates() {
-        if (!mUserInfoSubscription.isUnsubscribed()) 
+        if (!mUserInfoSubscription.isUnsubscribed())
             mUserInfoSubscription.unsubscribe();
-        
+
         if (!mTemperatureDeviceSubscription.isUnsubscribed())
             mTemperatureDeviceSubscription.unsubscribe();
 
-        if (mDevice != null) 
+        if (mDevice != null)
             RelayrSdk.getWebSocketClient().unSubscribe(mDevice.id);
     }
 
@@ -189,7 +221,8 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
     private void subscribeForTemperatureUpdates(TransmitterDevice device) {
         mDevice = device;
         RelayrSdk.getWebSocketClient().subscribe(device)
-                .subscribe(new Subscriber<Object>() {
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Reading>() {
                     @Override
                     public void onCompleted() {
                     }
@@ -198,27 +231,14 @@ public class ThermometerDemoActivity extends Activity implements LoginEventListe
                     public void onError(Throwable e) {
                         Toast.makeText(ThermometerDemoActivity.this, R.string.something_went_wrong,
                                 Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
                     }
 
                     @Override
-                    public void onNext(Object o) {
-                        Reading reading = new Gson().fromJson(o.toString(), Reading.class);
-                        mTemperatureValueTextView.setText(reading.temp + "˚C");
+                    public void onNext(Reading reading) {
+                        if (reading.meaning.equals("temperature"))
+                            mTemperatureValueTextView.setText(reading.value + "˚C");
                     }
                 });
-    }
-
-
-    @Override
-    public void onSuccessUserLogIn() {
-        Toast.makeText(this, R.string.successfully_logged_in, Toast.LENGTH_SHORT).show();
-        invalidateOptionsMenu();
-        updateUiForALoggedInUser();
-    }
-
-    @Override
-    public void onErrorLogin(Throwable e) {
-        Toast.makeText(this, R.string.unsuccessfully_logged_in, Toast.LENGTH_SHORT).show();
-        updateUiForANonLoggedInUser();
     }
 }
